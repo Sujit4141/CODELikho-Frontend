29df52b570765feb3ba185e5500ebc1632d6a360
Sujit4141 <jitsuofficial41@gmail.com>
2025-07-26 17:14:52 +0530

    Update Navbar.jsx

--- componenets/Navbar.jsx
+++ componenets/Navbar.jsx
@@ -69,7 +69,7 @@ function Navbar() {
           <div className="dropdown dropdown-end">
             <div tabIndex={0} role="button" className="btn btn-ghost btn-circle p-0">
               <div className="w-10 rounded-full border-2 border-amber-400">
-                <img src="/componenets/hacker.png" alt="User profile" className="object-cover" />
+                <img src="https://github.com/Sujit4141/CODELikho-Frontend/blob/main/componenets/hacker.png" alt="User profile" className="object-cover" />
               </div>
             </div>
             <ul tabIndex={0} className="dropdown-content menu shadow bg-gray-700 rounded-box w-52 mt-2">
